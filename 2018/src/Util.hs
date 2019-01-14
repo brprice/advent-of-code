bdbf1c162405a6edc0f5d15f9ed865120aefdb27
Ben Price <ben@brprice.uk>
2019-01-14 14:29:08 +0000

    Make Util.hs -Wall clean

--- 2018/src/Util.hs
+++ 2018/src/Util.hs
@@ -6,10 +6,10 @@ import Text.Megaparsec
 type Parser r = Parsec Void String r
 
 generic_main :: Show e => FilePath -> Parser a -> (a -> Either e b) -> (b -> String) -> IO ()
-generic_main f parse solver print = (putStrLn . go) =<< (runParser (parse <* eof) f <$> readFile f)
+generic_main f parser solver printer = (putStrLn . go) =<< (runParser (parser <* eof) f <$> readFile f)
   where go (Left e) = errorBundlePretty e
         go (Right a) = case solver a of
                          Left e -> show e
-                         Right b -> print b
+                         Right b -> printer b
 
 
