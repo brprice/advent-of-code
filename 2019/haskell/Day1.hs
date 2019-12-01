058e9f942d03b0b21c7711cd70f321281d46f01f
Ben Price <ben@brprice.uk>
2019-12-01 14:41:39 +0000

    2019: Haskell day 1b

--- 2019/haskell/Day1.hs
+++ 2019/haskell/Day1.hs
@@ -5,9 +5,16 @@ import Data.ByteString.Char8 (readInt)
 import Data.Maybe (fromJust)
 
 main :: IO ()
-main = do tot <- runConduitRes $ sourceFile "../data/day1a" .| linesUnboundedAsciiC .| mapC (fst . fromJust . readInt) .| mapC toFuel .| sumC
-          putStr "Total fuel needed: "
-          print tot
+main = do (simple,tyrant) <- runConduitRes $ sourceFile "../data/day1a"
+                                          .| linesUnboundedAsciiC
+                                          .| mapC (fst . fromJust . readInt)
+                                          .| mapC toFuel
+                                          .| foldlC (\(as,at) (s,t) -> (as+s,at+t)) (0,0)
+          putStr "Total fuel needed (simplistic, part a): "
+          print simple
+          putStr "Total fuel needed (tyrannical, part b): "
+          print tyrant
 
-toFuel :: Int -> Int
-toFuel mass = mass `div` 3 - 2
+toFuel :: Int -> (Int,Int)
+toFuel mass = let iters = tail $ iterate (\m -> m`div`3-2) mass
+              in (head iters, sum $ takeWhile (>0) iters)
