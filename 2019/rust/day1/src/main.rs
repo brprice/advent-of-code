0ebfb97479ebde5a193197a6add8221de8de8675
Ben Price <ben@brprice.uk>
2019-12-01 13:58:44 +0000

    2019: Rust day 1b

--- 2019/rust/day1/src/main.rs
+++ 2019/rust/day1/src/main.rs
@@ -6,12 +6,23 @@ fn main() {
     let path = Path::new("../../data/day1a");
     let file = File::open(path).unwrap();
     let lines = BufReader::new(file).lines();
-    let tot : i32 = lines.map(|l| to_fuel(l.unwrap())).sum();
+    let tot: (i32, i32) = lines.fold((0, 0), |acc, l| {
+        let f = to_fuel(l.unwrap());
+        (acc.0 + f.0, acc.1 + f.1)
+    });
 
-    println!("Total Fuel needed: {}",tot);
-} 
+    println!("Total Fuel needed (simplistic, part a): {}", tot.0);
+    println!("Total Fuel needed (tyrannical, part b): {}", tot.1);
+}
 
-fn to_fuel(s : String) -> i32 {
-    let mass : i32 = s.parse().unwrap();
-    mass/3 - 2
+fn to_fuel(s: String) -> (i32, i32) {
+    let mass: i32 = s.parse().unwrap();
+    let simplistic = mass / 3 - 2;
+    let mut tmp = simplistic;
+    let mut tyrannical = 0;
+    while tmp > 0 {
+        tyrannical += tmp;
+        tmp = tmp / 3 - 2;
+    }
+    (simplistic, tyrannical)
 }
