7ed380cda456dbde8250e6c77e9872fa2b234400
Ben Price <ben@brprice.uk>
2019-12-02 14:53:40 +0000

    2019: Rust day 2b

--- 2019/rust/day2/src/main.rs
+++ 2019/rust/day2/src/main.rs
@@ -30,23 +30,37 @@ impl From<std::num::ParseIntError> for Err {
 fn main() -> Result<(), Err> {
     let path = Path::new("../../data/day2");
     let file = File::open(path)?;
-    let mut ops: Vec<u32> = BufReader::new(file)
+    let ops: Vec<u32> = BufReader::new(file)
         .split(b',')
         .map(|x| Ok(String::from_utf8(x?)?.trim().parse()?))
         .collect::<Result<_, Err>>()?;
 
-    ops[1] = 12;
-    ops[2] = 2;
+    let parta = run(12, 2, &mut ops.clone());
 
-    run(&mut ops);
+    println!("part a: output for 1202 run is {}", parta);
 
-    println!("value at position 0, for 1202 run is {}", ops[0]);
+    let target = 19690720;
+    println!(
+        "part b: now searching for noun/verb pair giving output of {}",
+        target
+    );
+    'n: for n in 0..=99 {
+        for v in 0..=99 {
+            if run(n, v, &mut ops.clone()) == target {
+                println!("part b: found noun = {}, verb = {}", n, v);
+                println!("part b: thus answer is {}", 100 * n + v);
+                break 'n;
+            }
+        }
+    }
 
     Ok(())
 }
 
 // who needs error handling?
-fn run(ops: &mut Vec<u32>) -> () {
+fn run(noun: u32, verb: u32, ops: &mut Vec<u32>) -> u32 {
+    ops[1] = noun;
+    ops[2] = verb;
     let mut ip = 0;
     let mut i = ops[ip];
     while i != 99 {
@@ -56,10 +70,10 @@ fn run(ops: &mut Vec<u32>) -> () {
         match i {
             1 => ops[d] = ops[a] + ops[b],
             2 => ops[d] = ops[a] * ops[b],
-            99 => return,
             _ => panic!("Invalid opcode: {}", i),
         }
         ip += 4;
         i = ops[ip];
     }
+    ops[0]
 }
