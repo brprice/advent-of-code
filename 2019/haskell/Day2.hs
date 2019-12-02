73fb2d803234d326250d20b04be0efb96153092a
Ben Price <ben@brprice.uk>
2019-12-02 15:34:13 +0000

    2019: Haskell day 2b

--- 2019/haskell/Day2.hs
+++ 2019/haskell/Day2.hs
@@ -9,11 +9,18 @@ import qualified Data.Vector.Unboxed.Mutable as MV
 main :: IO ()
 main = do cts <- readFile "../data/day2"
           let ops = V.fromList $ map read $ words $ map (\c -> if c==',' then ' ' else c) cts :: V.Vector Int
-          let result = V.modify (\v -> MV.write v 1 12 >> MV.write v 2 2 >> run v) ops
+          let result = V.modify (run 12 2) ops
           putStrLn $ "part a: running in the 1202 configuration gives output: " ++ show (result V.! 0)
 
-run :: forall s . V.MVector s Int -> ST s ()
-run ops = go 0
+          let target = 19690720
+          putStrLn $ "part b: now searching for noun/verb pair giving output of " ++ show target
+          let nvs = [(n,v) | n<-[0..99],v<-[0..99], V.modify (run n v) ops V.! 0 == target]
+          let (n,v) = head nvs
+          putStrLn $ "part b: found noun = " ++ show n ++ ", verb = " ++ show v
+          putStrLn $ "part b: thus answer is " ++ show (100*n+v)
+
+run :: forall s . Int -> Int -> V.MVector s Int -> ST s ()
+run n v ops = MV.write ops 1 n >> MV.write ops 2 v >> go 0
   where go :: Int -> ST s ()
         go ip = do op <- MV.read ops ip
                    d <- MV.read ops (ip+3)
