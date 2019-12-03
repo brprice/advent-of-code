3ae39cb1abff25c2873f7999dc719cc50f700eed
Ben Price <ben@brprice.uk>
2019-12-03 19:42:52 +0000

    2019: Haskell day 3b

--- 2019/haskell/Day3.hs
+++ 2019/haskell/Day3.hs
@@ -35,6 +35,9 @@ toSegs = go $ Point 0 0
   where go _ [] = []
         go p ((d,l):sgs) = Seg p d l : go (add p d l) sgs
 
+toSteps :: [Seg] -> [(Int,Seg)]
+toSteps sgs = zip (scanl (\st sg -> st + abs (len sg)) 0 sgs) sgs
+
 inside :: Int -> (Int,Int) -> Bool
 inside x (a,b) = (a <= x && x <= b) || (b <= x && x <= a)
 
@@ -53,12 +56,19 @@ int (Seg s1 V l1) (Seg s2 H l2) = let x = px s1
                                      else Nothing
 int _ _ = Nothing -- Assume wire segments never overlap if they are parallel
 
+l1dist :: Point -> Point -> Int
+l1dist p q = abs (px p - px q) + abs (py p - py q)
+
 l1norm :: Point -> Int
-l1norm p = abs (px p) + abs (py p)
+l1norm = l1dist (Point 0 0)
 
 main :: IO ()
 main = do cts <- readFile "../data/day3"
-          let [w1,w2] = map (toSegs . parse) $ lines cts
-          let ints = catMaybes [int l1 l2 | l1 <- w1, l2<-w2]
-          let (minl1,minInt) = minimumOn fst $ map (\p -> (l1norm p,p)) ints
+          let [w1,w2] = map (toSteps . toSegs . parse) $ lines cts
+          mapM_ print $ take 20 w1
+          let intSt (s1,l1) (s2,l2) = (\i -> (i,s1 + s2 + l1dist i (start l1) + l1dist i (start l2))) <$> int l1 l2
+          let ints = catMaybes [intSt l1 l2 | l1 <- w1, l2<-w2]
+          let (minl1,minInt) = minimumOn fst $ map (\(p,_) -> (l1norm p,p)) ints
           putStrLn $ "Part a: the closest intersection was " ++ show minInt ++ " of l1 norm " ++ show minl1
+          let (leastStepsInt, leastSteps) = minimumOn snd ints
+          putStrLn $ "Part b: the intersection with least combined path-length was " ++ show leastStepsInt ++ " of combined path-length " ++ show leastSteps
