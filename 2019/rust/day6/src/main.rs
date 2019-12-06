4d752b7e94787fc2359da76a646967ee71fbf816
Ben Price <ben@brprice.uk>
2019-12-06 14:51:13 +0000

    2019: Rust day 6b

--- 2019/rust/day6/src/main.rs
+++ 2019/rust/day6/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::hash_map::HashMap;
+use std::collections::hash_set::HashSet;
 use std::fs;
+use std::hash::Hash;
 use std::iter::{empty, once, Iterator};
 
 fn main() {
@@ -9,11 +11,17 @@ fn main() {
         (x.next().unwrap(), x.next().unwrap())
     });
     let mut tree: HashMap<&str, Vec<&str>> = HashMap::new();
+    // the tree, but with edges pointing towards the root (for part b)
+    let mut eert: HashMap<&str, &str> = HashMap::new();
     for (sun, planet) in orbs {
         let ps = tree.entry(sun).or_insert(Vec::new());
         ps.push(planet);
-    }
 
+        eert.insert(planet, sun);
+    }
+    let eert = eert; // set non-mutable
+
+    // part a
     let mut total_orbits = 0;
     let mut level = 0;
     let mut cur: Box<dyn Iterator<Item = &str>> = Box::new(once("COM"));
@@ -35,4 +43,31 @@ fn main() {
         cur = Box::new(next);
     }
     println!("part a: total orbits: {}", total_orbits);
+
+    // part b
+    let com = {
+        let mut tmp = HashSet::new();
+        tmp.insert("COM");
+        tmp
+    };
+    let santa_path = path_to_set(&eert, &com, "SAN").unwrap();
+    let you_path = path_to_set(&eert, &santa_path.iter().map(|x| *x).collect(), "YOU").unwrap();
+    let common_ancestor = you_path.last().unwrap();
+    let santa_path = santa_path.split(|p| p == common_ancestor).next().unwrap(); // NB: doesn't contain common_ancestor
+    let transfers_needed = you_path.len() - 2 + santa_path.len() - 1;
+    println!("part b: transfers needed: {}", transfers_needed);
+}
+
+fn path_to_set<'a, T: Eq + Hash + ?Sized>(
+    tree: &HashMap<&'a T, &'a T>, // upward edges
+    set: &HashSet<&'a T>,
+    node: &'a T,
+) -> Option<Vec<&'a T>> {
+    let mut cur = node;
+    let mut path = vec![cur];
+    while !set.contains(cur) {
+        cur = tree[cur];
+        path.push(cur);
+    }
+    Some(path)
 }
