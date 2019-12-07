21fcc95c432b36c60310f7374cfffc1bbf30caea
Ben Price <ben@brprice.uk>
2019-12-07 20:33:08 +0000

    2019: Haskell day 7b

--- 2019/haskell/Day7.hs
+++ 2019/haskell/Day7.hs
@@ -91,10 +91,29 @@ run input = do op <- getOp
         c_bool True = 1
         c_bool False = 0
 
+rotateR :: [a] -> [a]
+rotateR xs = last xs : init xs
+
+-- Like zipWith, but assumes the first input list is the shorter of the two
+-- and thus the output is the same length as the first input list
+-- We require this extra laziness, otherwise part b <<loop>>s, because of the line
+-- oss = zipWith phased ps $ rotateR oss
+zipWithL :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithL _ [] _ = []
+zipWithL f (x:xs) ~(y:ys) = f x y : zipWithL f xs ys
+
 main :: IO ()
 main = do cts <- readFile "../data/day7"
           let ops = IM.fromList $ zip [0..] $ map read $ words $ map (\c -> if c==',' then ' ' else c) cts :: IM.IntMap Int
-          let phased p i = head $ evalState (run [p,i]) (IC {ip=0,mem=ops})
-          let chained ps = appEndo $ getDual $ foldMap (Dual . Endo . phased) ps
+          let phased p is = evalState (run $ p:is) (IC {ip=0,mem=ops})
+
+          let phased1 p i = head $ phased p [i]
+          let chained ps = appEndo $ getDual $ foldMap (Dual . Endo . phased1) ps
           putStr "part a: maximum thrusters "
           print $ maximum $ map (flip chained 0) $ permutations [0..4]
+
+          let looped ps i = let oss :: [[Int]]
+                                oss = zipWithL phased ps $ zipWith (++) ([i] : repeat []) $ rotateR oss
+                            in last $ last oss
+          putStr "part b: maximum loop-amplified thrusters "
+          print $ maximum $ map (flip looped 0) $ permutations [5..9]
