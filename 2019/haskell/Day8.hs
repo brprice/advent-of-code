d45b0d570bdb980d09103e2ad840f04a98f295ff
Ben Price <ben@brprice.uk>
2019-12-09 00:30:59 +0000

    2019: Haskell day 8b

--- 2019/haskell/Day8.hs
+++ 2019/haskell/Day8.hs
@@ -1,11 +1,22 @@
 module Main where
 
+import Data.List (foldl')
 import Data.List.Extra (minimumOn)
 
 chunks :: Int -> [a] -> [[a]]
 chunks _ [] = []
 chunks n xs = let (h,t) = splitAt n xs
               in h : chunks n t
+
+data Colour = Black
+            | White
+            | Alpha
+  deriving (Eq,Enum)
+
+charColour :: Colour -> Char
+charColour Black = ' '
+charColour White = '#'
+charColour Alpha = '-'
 
 main :: IO()
 main = do dat <- readFile "../data/day8"
@@ -17,3 +28,11 @@ main = do dat <- readFile "../data/day8"
           let parta = length (filter (==1) best_layer) * length (filter (==2) best_layer)
           putStr "part a: "
           print parta
+
+          let layerColors = map (map toEnum) layers
+          let alpha = replicate (w*h) Alpha
+          let dbg lay = unlines $ chunks w $ map charColour lay
+          let super img lay = zipWith (\ic lc -> if ic == Alpha then lc else ic) img lay
+          let img = foldl' (\i l -> super i l) alpha $ layerColors
+          putStrLn "part b:"
+          mapM_ (\row -> putStrLn $ map charColour row) $ chunks w img
