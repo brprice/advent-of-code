dfd15f8f1cbada5d019cde1081009ebf4d247ee4
Ben Price <ben@brprice.uk>
2019-12-09 21:35:44 +0000

    2019: Haskell day 9b

--- 2019/haskell/Day9.hs
+++ 2019/haskell/Day9.hs
@@ -8,3 +8,6 @@ main :: IO ()
 main = do mach <- readIntcode "../data/day9"
           putStr "part a: "
           print $ head $ evalState (run [1]) mach
+
+          putStr "part b: "
+          print $ head $ evalState (run [2]) mach
