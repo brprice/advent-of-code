b1d5c20b7b28dd5447570a08492adf89c0fa1ca0
Ben Price <ben@brprice.uk>
2019-12-09 00:27:25 +0000

    2019: Rust day 8b

--- 2019/rust/day8/src/main.rs
+++ 2019/rust/day8/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[derive(Clone)]
 struct Chunked<T> {
     size: usize,
     source: T,
@@ -28,6 +29,13 @@ fn chunks<I: Iterator>(source: I, size: usize) -> Chunked<I> {
     Chunked { size, source }
 }
 
+#[derive(Clone, Copy)]
+enum Colour {
+    Black,
+    White,
+    Alpha,
+}
+
 fn main() {
     let dat = fs::read_to_string("../../data/day8").unwrap();
     let pix = dat.trim().chars().map(|x| x.to_digit(10).unwrap());
@@ -44,5 +52,39 @@ fn main() {
             2 => (os, ts + 1),
             _ => (os, ts),
         });
-    println!("{}", ones * twos);
+    println!("part a: {}", ones * twos);
+
+    let dat = fs::read_to_string("../../data/day8").unwrap();
+    let layers = chunks(
+        dat.trim().chars().map(|x| match x {
+            '0' => Colour::Black,
+            '1' => Colour::White,
+            '2' => Colour::Alpha,
+            _ => panic!("unknown colour {}", x),
+        }),
+        w * h,
+    );
+    let mut final_img = Vec::with_capacity(w * h);
+    for _ in 0..w * h {
+        final_img.push(Colour::Alpha)
+    }
+    for l in layers {
+        for i in 0..w * h {
+            match final_img[i] {
+                Colour::Alpha => final_img[i] = l[i],
+                _ => {}
+            }
+        }
+    }
+    println!("part b:");
+    for y in 0..h {
+        for x in 0..w {
+            match final_img[y * w + x] {
+                Colour::Black => print!(" "),
+                Colour::White => print!("#"),
+                Colour::Alpha => print!("-"),
+            }
+        }
+        print!("\n");
+    }
 }
