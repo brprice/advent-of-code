3ada1aa361c5f39b78904004fc4d169f3eead95c
Ben Price <ben@brprice.uk>
2019-12-25 01:39:54 +0000

    2019: Rust day 21b

--- 2019/rust/src/bin/day21.rs
+++ 2019/rust/src/bin/day21.rs
@@ -1,11 +1,16 @@
 use aoc2019::intcode::*;
 use aoc2019::sparse::*;
 
+/* Today was a "jump over the holes" game - you either move forward one square, or jump over three.
+ * It seems impossible to ensure success (i.e. not falling into a hole) with finite look-ahead, but
+ * these strategies work for my inputs */
+
 fn main() {
     let mem: Sparse<usize, i64> = read_intcode_sparse("../data/day21");
-    let mut mach = IC::new(0, mem);
+    let mach = IC::new(0, mem);
+    let mut macha = mach.clone();
     // jump if there is land 4 tiles away, and a hole before that
-    let inp = "NOT A T
+    let inpa = "NOT A T
 OR T J
 NOT B T
 OR T J
@@ -14,7 +19,27 @@ OR T J
 AND D J
 WALK
 ";
-    let mut inp = inp.chars().map(|c| c as i64);
-    let out = mach.run(&mut inp);
-    println!("{}", out.last().unwrap());
+    let mut inpa = inpa.chars().map(|c| c as i64);
+    let outa = macha.run(&mut inpa);
+    println!("{}", outa.last().unwrap());
+
+    let mut machb = mach;
+    // Jump if you can land, it isn't immediate doom, and you must soon
+    // (i.e. an optimised version of part a, plus a bit more look-ahead)
+    // D && (E || H) && (!A || !B || !C)
+    let inpb = "OR E J
+OR H J
+AND D J
+NOT T T
+AND A T
+AND B T
+AND C T
+NOT T T
+AND T J
+RUN
+";
+
+    let mut inpb = inpb.chars().map(|c| c as i64);
+    let outb = machb.run(&mut inpb);
+    println!("{}", outb.last().unwrap());
 }
