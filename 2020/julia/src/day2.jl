cbe0486c2e010833edad39949c5039808130988a
Ben Price <ben@brprice.uk>
2020-12-02 17:48:50 +0000

    2020: Julia day 2a
    Ran into my first bug because I messed up some types. In 'isValid', I
    was doing 'count(x->(x==c))' as in the commit, but I had 'c' being a
    string returned from 'parsePw', rather than a character. Since 'a'!="a",
    we then counted 0 valid entries. This was thankfully fairly easy to
    spot, but I really wish the computer could have given me a hand!

---
+++ 2020/julia/src/day2.jl
@@ -0,0 +1,26 @@
+function parsePw(p)
+  #format is "1-3 c: aaac" from which we extract (1,3,'c',"aaac")
+  m=match(r"^([[:digit:]]+)-([[:digit:]]+) (.): ([[:alpha:]]*)$",p)
+  (l,h,c,pw) = m.captures
+  return (parse(Int,l),parse(Int,h),c[1],pw)
+end
+
+function isValid(l,h,c,pw)
+  l <= count(x->(x==c),pw) <= h
+end
+
+function day2a(passwords)
+  valid=0
+  for p in passwords
+    (l,h,c,pw) = parsePw(p)
+    if isValid(l,h,c,pw)
+      valid+=1
+    end
+  end
+  return valid
+end
+
+passwords = readlines("../../data/day2")
+
+print("Day 2a: ")
+println(day2a(passwords))
