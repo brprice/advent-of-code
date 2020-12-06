713e370036e5b355b4025cc911785150ccd78bcb
Ben Price <ben@brprice.uk>
2020-12-06 11:54:54 +0000

    2020: Julia day 6b
    Same as part a, but intersection rather than union. This made it
    slightly more awkward syntactically, as I wanted to take the
    intersection using Sets, rather than Vectors, and the identity element
    is thus the full set rather than the empty set. To avoid this, let's
    just start off with the Set constructed from the first person, which
    leads to the syntactical awkwardness.

--- 2020/julia/src/day6.jl
+++ 2020/julia/src/day6.jl
@@ -2,6 +2,10 @@ function day6a(groups)
   sum(g->length(union(Set(""),g...)),groups)
 end
 
+function day6b(groups)
+  sum(g->length(intersect(Set(g[1]),g[2:end]...)),groups)
+end
+
 data = []
 open("../../data/day6","r") do io
   while !eof(io)
@@ -12,3 +16,4 @@ open("../../data/day6","r") do io
 end
 
 println("Day 6a: ",day6a(data))
+println("Day 6b: ",day6b(data))
