691aded573c6b722d2211ab5ffa5f0074bc6b93e
Ben Price <ben@brprice.uk>
2020-12-09 20:40:14 +0000

    2020: Julia day 9a, naive method
    I thought I'd compare how my clever method fared to brute-force. Poorly,
    it turns out!

--- 2020/julia/src/day9.jl
+++ 2020/julia/src/day9.jl
@@ -20,6 +20,30 @@ function day9a(data,n)
   error("Didn't find a \"bad\" datapoint")
 end
 
+function day9aNaive(data,n)
+  for i in n+1:length(data)
+    isSum=false
+    di=data[i]
+    for j in 1:25
+      if isSum
+        break
+      end
+      dj=data[i-j]
+      for k in 1:j-1
+        dk=data[i-k]
+        if dj != dk && dj+dk==di
+          isSum=true
+          break
+        end
+      end
+    end
+    if !isSum
+      return di
+    end
+  end
+  error("Didn't find a \"bad\" datapoint")
+end
+
 const Cache = Vector{Tuple{Int,Set{Int}}}
 function emptyCache() :: Cache
   Cache()
@@ -44,4 +68,5 @@ end
 
 data = map(x->parse(Int,x),readlines("../../data/day9"))
 
-println("Day 9a: ",day9a(data,25))
+@time println("Day 9a (\"smart\" method): ",day9a(data,25))
+@time println("Day 9a (naive method): ",day9aNaive(data,25))
