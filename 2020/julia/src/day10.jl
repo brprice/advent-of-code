84781971aae12b780356fe9635140dfb8726d654
Ben Price <ben@brprice.uk>
2020-12-10 22:46:36 +0000

    2020: Julia day 10b

--- 2020/julia/src/day10.jl
+++ 2020/julia/src/day10.jl
@@ -1,5 +1,4 @@
 function day10a(data)
-  sort!(data)
   last = 0
   ones = 0
   threes = 0
@@ -14,6 +13,18 @@ function day10a(data)
   return ones*(threes+1)
 end
 
+function day10b(data)
+  numPathsTo = Dict(0=>1)
+  target = data[end]+3
+  push!(data,target)
+  for i in data
+    numPathsTo[i] = get(numPathsTo, i-3, 0) + get(numPathsTo, i-2, 0) + get(numPathsTo, i-1, 0)
+  end
+  return numPathsTo[target]
+end
+
 data = map(x->parse(Int,x),readlines("../../data/day10"))
+sort!(data)
 
 println("Day 10a: ",day10a(data))
+println("Day 10b: ",day10b(data))
