0c3c7d3a37bddaa7633ab75c319aa27608140c27
Ben Price <ben@brprice.uk>
2020-12-13 12:47:22 +0000

    2020: Julia day 13b
    I'm glad I thought ahead and didn't just drop the 'x's from the input!

--- 2020/julia/src/day13.jl
+++ 2020/julia/src/day13.jl
@@ -1,9 +1,41 @@
+# Bus with id i leaves at times divisible by i
+# Find the next bus to leave
+# (if i=0, ignore it)
 function day13a(time,ids0)
   ids = filter(x->x!=0,ids0)
   tis = map(i->(i-time%i,i),ids)
   t, i = minimum(tis)
   i*t
 end
+
+# For a list of ids i_n (zero-indexed), find the least t such that
+# each bus departs at t+n.
+# A bus with id i leaves at times divisible by i, if i=0, no restriction on
+# busses departing at t+n
+function day13b(ids)
+# This is just chinese remainder theorem
+# (We assume that the ids are pairwise coprime)
+  constraints = []
+  for (d,i) in enumerate(ids)
+    if i != 0
+      push!(constraints,(-(d-1),i))
+    end
+  end
+  cr(constraints)
+end
+
+# If cs = [(a_1,n_1),...], solve the simultaneous equations
+# x = a_i (mod n_i)
+# We assume the n_i are pairwise coprime
+function cr(cs)
+# This is the simple, direct construction, rather than the more
+# efficient inductive do-a-pair-of-equations-at-a-time solution.
+# More than good enough for what we are doing.
+  N = prod(map(x->x[2],cs))
+  x = sum(map(an->an[1]*div(N,an[2])*invmod(div(N,an[2]),an[2]),cs))
+  mod(x,N)
+end
+
 
 # we convert 'x' to 0
 data = readlines("../../data/day13")
@@ -16,3 +48,4 @@ ids = map(split(data[2],",")) do x
 end
 
 println("Day 13a: ",day13a(time,ids))
+println("Day 13b: ",day13b(ids))
