47f859d0567fc08898385c0c6cfbff18cb738e8d
Ben Price <ben@brprice.uk>
2020-12-15 10:09:38 +0000

    2020: Julia day 15b
    I'm surprised that that the index for part b was small enough for the
    same solution to work well (~2 seconds) - I was expecting to have to
    need an insight for a better algorithm!

--- 2020/julia/src/day15.jl
+++ 2020/julia/src/day15.jl
@@ -1,13 +1,14 @@
 # We speak numbers in a sequence, starting with a given prelude
 # after that, a_{n+1} = 0 if a_n != a_i for all i<n
 # and a_{n+1} = n-i if a_i == a_n and a_j != a_n for each i<j<n
-# What is a_2020?
-function day15a(prelude)
+# part a: What is a_2020?
+# part b: What is a_30000000?
+function day15(prelude,N)
 # We maintain a dictionary of the last time we spoke each number
   last = Dict(map(x->(x[2],x[1]),enumerate(prelude[1:end-1])))
   a = prelude[end]
   n = length(prelude)
-  while n < 2020
+  while n < N
     b = n - get(last,a,n)
     last[a] = n
     a = b
@@ -18,4 +19,5 @@ end
 
 data = map(n->parse(Int,n),split(read("../../data/day15",String),","))
 
-println(day15a(data))
+println("a_2020: ",day15(data,2020))
+println("a_30000000: ",day15(data,30000000))
