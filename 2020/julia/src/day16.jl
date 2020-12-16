0e0a46465ebfc87b0293044c521afdda0d95f5cf
Ben Price <ben@brprice.uk>
2020-12-16 12:45:56 +0000

    2020: Julia day 16b

--- 2020/julia/src/day16.jl
+++ 2020/julia/src/day16.jl
@@ -16,8 +16,47 @@ function day16a(rules,tickets)
   return invalid
 end
 
+function isValidTicket(rules,t)
+  all(n->any(r->isValid(r,n),rules),t)
+end
+
 function isValid((field,rules),n)
   any(rng->in(n,rng),rules)
+end
+
+function day16b(rules,yourTicket,tickets0)
+# We take a simple approach iteratively finding one field at a time which
+# we can deduce, then removing that possibility from consideration for the
+# others
+  tickets1 = filter(t->isValidTicket(rules,t),tickets0)
+  tickets = hcat(tickets1...) # each ticket is now a column of this matrix
+  possFields = []
+  numFields = size(tickets)[1]
+  @assert numFields == length(rules) "number of fields mismatch"
+  for i in 1:numFields
+    fieldEnts = tickets[i,:]
+    consistentFields = map(r->r[1],filter(r->all(n->isValid(r,n),fieldEnts),rules))
+    push!(possFields,Set(consistentFields))
+  end
+  known = Dict()
+  while length(known) < numFields
+    for i in 1:numFields
+      if length(possFields[i]) == 1
+        found = pop!(possFields[i])
+        known[i] = found
+        for j in 1:numFields
+          delete!(possFields[j],found)
+        end
+        break
+      end
+    end
+  end
+  departureFields = filter(r->startswith(r[2],"departure"),known)
+  ans = 1
+  for k in keys(departureFields)
+    ans *= yourTicket[k]
+  end
+  return ans
 end
 
 rules = []
@@ -55,3 +94,4 @@ open("../../data/day16","r") do file
 end
 
 println(day16a(rules,otherTickets))
+println(day16b(rules,yourTicket,otherTickets))
