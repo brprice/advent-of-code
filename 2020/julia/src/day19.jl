e95c8c5df9a049aeec2f8cb467f5d0e9a2ca96af
Ben Price <ben@brprice.uk>
2020-12-19 17:19:16 +0000

    2020: Julia day 19a
    We write a simple "a parser is a map from strings to pairs of things and
    strings" solution.

---
+++ 2020/julia/src/day19.jl
@@ -0,0 +1,63 @@
+function day19a(rules,msgs)
+  count(m->matches(m,rules,0),msgs)
+end
+
+function matches(m,rules,i)
+  any(match(m,rules,i)) do m
+    isempty(m[2])
+  end
+end
+
+function match(m,rules,i)
+  r = rules[i]
+  if typeof(r) == Char
+    if m[1] == r
+      return [(m[1]^1,m[2:end])]
+    else
+      return []
+    end
+  else
+    alts = map(a -> matchlist(m,rules,a),r)
+    vcat(alts...)
+  end
+end
+
+function matchlist(m,rules,is)
+  if isempty(is)
+    return [("",m)]
+  end
+  ms = match(m,rules,is[1])
+  ret = []
+  for (matched,rest) in ms
+    for (matched2,rest2) in matchlist(rest,rules,is[2:end])
+      push!(ret,(matched*matched2,rest2))
+    end
+  end
+  return ret
+end
+
+# We know that a rule is either of the form
+# 1 : "a"  which is a rule numbered '1' and matches a literal character 'a'
+# or
+# 2 : 1 4 | 3 which is a rule numbered '2' and matches if either the rule 1
+# matches a prefix, and the rest matches rule 4, or if rule 3 matches.
+# We represent the rule 0: 1 2 | 3  as 0=>[[1,2],[3]]
+rules = Dict{Int,Union{Char,Array{Array{Int,1},1}}}()
+open("../../data/day19","r") do h
+  rs = split(readuntil(h, "\n\n"),"\n")
+  for r in rs
+    name, rhs = split(r,": ")
+    i = parse(Int,name)
+    if rhs[1] == '"'
+      global rules[i] = rhs[2]
+    else
+      alts = map(split(rhs," | ")) do a
+        map(n->parse(Int,n),split(a," "))
+      end
+      global rules[i] = alts
+    end
+  end
+  global msgs = readlines(h)
+end
+
+println(day19a(rules,msgs))
