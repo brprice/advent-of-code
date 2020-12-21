a0f67859d95da4d0fa181e16fbcfdab28d98b34f
Ben Price <ben@brprice.uk>
2020-12-21 13:55:32 +0000

    2020: Julia day 21b

--- 2020/julia/src/day21.jl
+++ 2020/julia/src/day21.jl
@@ -3,8 +3,10 @@
 # a, b or c. We do NOT know that allergen Z is not in a, b or c.
 # We also know that allergens are in exactly one ingredient
 # (across all records), and each ingredient has 0 or 1 allergen.
-# Find which ingredients are allergen-free.
-function day21a(data)
+# Find which ingredients contain which allergens.
+# We return (d,s) where d is a dict mapping allergens to ingredient
+# and s is a set of safe ingredients
+function findAllergens(data)
 # Strategy: from (is1,as1) and (is2,as2) with X in as1 and X in as2
 # we know that X is contained in one of the ingredients in the
 # intersection of is1 and is2.
@@ -48,12 +50,21 @@ function day21a(data)
     end
   end
 
-  # How many times do allergen-free ingredients appear?
+  return (known,ingredients)
+end
+
+# How many times do allergen-free ingredients appear?
+function day21a(data,safe)
   total = 0
   for (is,_) in data
-    total += length(intersect(is,ingredients))
+    total += length(intersect(is,safe))
   end
   return total
+end
+
+# What are the dangerous ingredients?
+function day21b(allergens)
+  join(map(x->x[2],sort(collect(pairs(allergens)))),',')
 end
 
 data = map(readlines("../../data/day21")) do l
@@ -63,4 +74,6 @@ data = map(readlines("../../data/day21")) do l
   ingredients, allergens
 end
 
-println(day21a(data))
+allergens, safe = findAllergens(data)
+println(day21a(data,safe))
+println(day21b(allergens))
