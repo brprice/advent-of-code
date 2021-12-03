f7439049598c544c41df693e67cf126ccd8de74a
Ben Price <ben@brprice.uk>
2021-12-03 14:52:43 +0000

    2021: racket day 3b
    We rejig part a to take its input as lists of bits, rather than strings
    (since this is what part b needs also). We also refactor part a to use
    some utilities created for part b.

--- 2021/racket/day3.rkt
+++ 2021/racket/day3.rkt
@@ -3,22 +3,23 @@
 (define (with-data f)
   (call-with-input-file
     "../data/day3"
-    (lambda (p) (f (sequence->stream (in-lines p))))))
+    (lambda (p) (f (sequence->stream (sequence-map string->bits (in-lines p)))))))
 
 (define (with-test-data f)
-  (f (list
-       "00100"
-       "11110"
-       "10110"
-       "10111"
-       "10101"
-       "01111"
-       "00111"
-       "11100"
-       "10000"
-       "11001"
-       "00010"
-       "01010")))
+  (f (map string->bits
+	  (list
+	    "00100"
+            "11110"
+            "10110"
+            "10111"
+            "10101"
+            "01111"
+            "00111"
+            "11100"
+            "10000"
+            "11001"
+            "00010"
+            "01010"))))
 
 (define (string->bits s)
   (sequence-map (lambda (c) (string->number (string c))) s))
@@ -34,23 +35,54 @@
       (map (lambda (g acc) (g acc x)) (list f ...) accs))
     (list i ...) s))
 
-(define (part1 s)
+(define (majority-bits s #:eq [def 0])
   (match (fold-parallel
 	   ([pointwise-sum (in-cycle (in-value 0))]
 	    [(lambda (l _) (+ l 1)) 0])
-	   (sequence-map string->bits s))
+	   s)
 	 [(list count-1s lines)
-	  (match (sequence-fold (lambda (gd c1)
-				  (let* ([c0 (- lines c1)]
-					 [maj1 (> c1 c0)]
-				         [gb (if maj1 1 0)]
-				         [db (- 1 gb)]
-				         [g (+ (* 2 (car gd)) gb)]
-				         [d (+ (* 2 (cdr gd)) db)])
-				    (cons g d)))
-				(cons 0 0)
-				count-1s)
-		 [(cons gamma delta) (* gamma delta)])]))
+	  (sequence-map (lambda (c1) (let ([c0 (- lines c1)])
+				       (cond
+					 [(> c1 c0) 1]
+					 [(< c1 c0) 0]
+					 [else def])))
+			count-1s)]))
+
+(define (bits->number bs)
+  (sequence-fold (lambda (acc b) (+ (* 2 acc) b)) 0 bs))
+
+(define (part1 s)
+  (let* ([maj (majority-bits s)]
+	 [gamma (bits->number maj)]
+	 [delta (bits->number (sequence-map (lambda (b) (- 1 b)) maj))])
+    (* gamma delta)))
 
 (printf "part 1: ~a\n"
 	(with-data part1))
+
+(define (o2 s)
+  (bits->number
+    (letrec ([go (lambda (i s)
+		   (let* ([maj (majority-bits s #:eq 1)]
+			  [sf (sequence-filter
+				(lambda (n) (eq? (sequence-ref maj i) (sequence-ref n i)))
+				s)])
+		     (if (stream-empty? (stream-rest sf)) (stream-first sf) (go (+ 1 i) sf))))])
+      (go 0 s))))
+
+(define (co2 s)
+  (bits->number
+    (letrec ([go (lambda (i s)
+		   (let* ([maj (majority-bits s #:eq 1)]
+			  [sf (sequence-filter
+				(lambda (n) (eq? (- 1 (sequence-ref maj i)) (sequence-ref n i)))
+				s)])
+		     (if (stream-empty? (stream-rest sf)) (stream-first sf) (go (+ 1 i) sf))))])
+      (go 0 s))))
+
+; part 2: we don't worry about being efficient at all
+(define (part2 s)
+  (* (o2 s) (co2 s)))
+
+(printf "part 2: ~a\n"
+	(with-data part2))
