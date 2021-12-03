5eff2ba2f2134d762d879fa3a6380c10e1da5f8c
Ben Price <ben@brprice.uk>
2021-12-03 01:04:18 +0000

    2021: racket day 2b
    There is a bunch of simple refactoring, due to me not anticipating we
    would need to interpret the input differently in part b.

--- 2021/racket/day2.rkt
+++ 2021/racket/day2.rkt
@@ -1,11 +1,5 @@
 #lang racket
 
-(define (get-cmd cmd)
-  (match cmd
-	 ["forward" forward]
-	 ["up" up]
-	 ["down" down]))
-
 (define (with-data f)
   (call-with-input-file
     "../data/day2"
@@ -17,27 +11,52 @@
 	      (let* ([words (string-split s)]
 		     [cmd (car words)]
 		     [arg (cadr words)])
-		(cons (get-cmd cmd) (string->number arg))))
+		(cons cmd (string->number arg))))
 	    (in-lines p)))))))
 
 (define (with-test-data f)
   (f (list
-       (cons forward 5)
-       (cons down 5)
-       (cons forward 8)
-       (cons up 3)
-       (cons down 8)
-       (cons forward 2))))
+       (cons "forward" 5)
+       (cons "down" 5)
+       (cons "forward" 8)
+       (cons "up" 3)
+       (cons "down" 8)
+       (cons "forward" 2))))
 
-; we will operate on pairs of horizontal position and depth
-(define (forward n p) (cons (+ (car p) n) (cdr p)))
-(define (up n p) (cons (car p) (- (cdr p) n)))
-(define (down n p) (cons (car p) (+ (cdr p) n)))
+; part 1: we will operate on pairs of horizontal position and depth
+(define (forward1 n p) (cons (+ (car p) n) (cdr p)))
+(define (up1 n p) (cons (car p) (- (cdr p) n)))
+(define (down1 n p) (cons (car p) (+ (cdr p) n)))
+
+(define (get-cmd1 cmd)
+  (match cmd
+	 ["forward" forward1]
+	 ["up" up1]
+	 ["down" down1]))
 
 (define (part1 s)
-  (let* ([f (lambda (acc cmd-arg) ((car cmd-arg) (cdr cmd-arg) acc))]
+  (let* ([f (lambda (acc cmd-arg) ((get-cmd1 (car cmd-arg)) (cdr cmd-arg) acc))]
 	 [target (sequence-fold f (cons 0 0) s)])
   (* (car target) (cdr target))))
 
 (printf "part 1: ~a\n"
 	(with-data part1))
+
+; part 2: we will operate on lists of horizontal position, depth and aim
+(define (forward2 n p) (list (+ (car p) n) (+ (cadr p) (* (caddr p) n)) (caddr p)))
+(define (up2 n p) (list (car p) (cadr p) (- (caddr p) n)))
+(define (down2 n p) (list (car p) (cadr p) (+ (caddr p) n)))
+
+(define (get-cmd2 cmd)
+  (match cmd
+	 ["forward" forward2]
+	 ["up" up2]
+	 ["down" down2]))
+
+(define (part2 s)
+  (let* ([f (lambda (acc cmd-arg) ((get-cmd2 (car cmd-arg)) (cdr cmd-arg) acc))]
+	 [target (sequence-fold f (list 0 0 0) s)])
+  (* (car target) (cadr target))))
+
+(printf "part 2: ~a\n"
+	(with-data part2))
