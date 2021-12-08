278ebea58b36a86dd89fc1f63f99603565547799
Ben Price <ben@brprice.uk>
2021-12-08 21:25:23 +0000

    2021: racket day 8b

--- 2021/racket/day8.rkt
+++ 2021/racket/day8.rkt
@@ -28,3 +28,31 @@
 
 (printf "part 1: ~a\n"
 	(with-data part1))
+
+; part 2: I manually did the analysis and worked out you can tell that, for
+; example "3" is the unique 5-segment number which has all the segments of "1"
+; illuminated. This code just runs this simple analysis.
+(define (part2digits in)
+  (let ([lits (sort (group-by string-length in) < #:key (lambda (g) (string-length (car g))))]
+	[ctg (lambda (small) (lambda (big) (andmap (lambda (sg) (member sg (string->list big))) (string->list small))))])
+    (match lits
+	   [(list (list one) (list seven) (list four) two-three-five zero-six-nine (list eight))
+	    (let* ([three (car (filter (ctg one) two-three-five))]
+		   [six (car (filter (compose not (ctg one)) zero-six-nine))]
+		   [five (car (filter (lambda (x) ((ctg x) six)) two-three-five))]
+		   [zero (car (filter (compose not (ctg five)) zero-six-nine))]
+		   [nine (car (filter (ctg four) zero-six-nine))]
+		   [two (car (filter (lambda (x) (not ((ctg x) nine))) two-three-five))])
+	      (list zero one two three four five six seven eight nine))])))
+
+(define (part2help io)
+  (let* ([srt (lambda (s) (sort (string->list s) char<?))]
+	 [digits (map srt (part2digits (car io)))]
+	 [out (map srt (cdr io))])
+    (foldl (lambda (d acc) (+ (* 10 acc) d)) 0 (map (curry index-of digits) out))))
+
+(define (part2 xs)
+  (sequence-fold + 0 (sequence-map part2help xs)))
+
+(printf "part 2: ~a\n"
+	(with-data part2))
