3e7461e807f81efcbd7a1c85d3416acf0c886e5f
Ben Price <ben@brprice.uk>
2021-12-09 18:13:30 +0000

    2021: racket day 9a

---
+++ 2021/racket/day9.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+(define (with-data f)
+  (call-with-input-file
+    "../data/day9"
+    (let ([g (lambda (l) (map (compose string->number list->string list)
+			      (string->list l)))])
+      (lambda (p)
+	(let* ([ls (in-lines p)]
+	       [l1 (g (sequence-ref ls 0))]
+	       [w (length l1)]
+	       [res (sequence-fold (lambda (acc l) (cons (add1 (car acc)) (append (cdr acc) (g l))))
+			 (cons 1 l1) ls)]
+	       [h (car res)]
+	       [v (list->vector (cdr res))])
+	(f (vec2 w h v)))))))
+
+(define (vec2 x y v)
+  (list x y (lambda (i j)
+	      (vector-ref v (+ i (* x j))))))
+
+(define (vec2-width xs) (car xs))
+(define (vec2-height xs) (cadr xs))
+(define (vec2-lookup xs i j) ((caddr xs) i j))
+
+(define (with-test-data f)
+  (let ([data #(2 1 9 9 9 4 3 2 1 0
+		3 9 8 7 8 9 4 9 2 1
+		9 8 5 6 7 8 9 8 9 2
+		8 7 6 7 8 9 6 7 8 9
+		9 8 9 9 9 6 5 6 7 8)]
+	[x 10]
+	[y 5])
+    (f (vec2 x y data))))
+
+(define (adjacent-idx xs i j)
+  (filter (lambda (a) (and (< -1 (car a) (vec2-width xs))
+			   (< -1 (cadr a) (vec2-height xs))))
+	  (list (list (sub1 i) j)
+		(list (add1 i) j)
+		(list i (sub1 j))
+		(list i (add1 j)))))
+
+
+(define (local-minimum xs i j)
+  (let ([adj (adjacent-idx xs i j)]
+	[here (vec2-lookup xs i j)])
+    (andmap (lambda (a)
+	      (< here (apply vec2-lookup xs a)))
+	    adj)))
+
+(define (part1 xs)
+  (for*/sum ([j (vec2-height xs)]
+	     [i (vec2-width xs)]
+	     #:when (local-minimum xs i j))
+	    (add1 (vec2-lookup xs i j))))
+
+(printf "part 1: ~a\n"
+	(with-data part1))
