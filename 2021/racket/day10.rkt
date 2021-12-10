824e8676c7ecd59d3963613d9e94a4d44b47b077
Ben Price <ben@brprice.uk>
2021-12-10 15:53:40 +0000

    2021: racket day 10b

--- 2021/racket/day10.rkt
+++ 2021/racket/day10.rkt
@@ -32,20 +32,20 @@ EOF
 
 ; either returns
 ; for an corrupted line (the second elt is the unexpected character #\) #\} #\> or #\]): (list 'corrupted #\))
-; for an incomplete line (list 'incomplete)
+; for an incomplete line (the second elt is the unmatched opening characters, right-most first) (list 'incomplete stk)
 (define (validate l)
-  (let ([ret '(incomplete)]
+  (let ([ret #f]
 	[stk '()])
     (for ([c l]
-	  #:break (not (equal? ret '(incomplete))))
+	  #:break ret)
 	 (if (is-open c)
 	   (set! stk (cons c stk))
 	   (if (eq? (matching-open c) (car stk))
 	     (set! stk (cdr stk))
 	     (set! ret (list 'corrupted c)))))
-    ret))
+    (if ret ret (list 'incomplete stk))))
 
-(define/match (score c)
+(define/match (score-corrupted c)
   [(#\)) 3]
   [(#\]) 57]
   [(#\}) 1197]
@@ -53,9 +53,29 @@ EOF
 
 (define (part1 xs)
   (stream-fold + 0
-	 (stream-map (compose score cadr)
-	      (stream-filter (lambda (r) (not (equal? '(incomplete) r)))
+	 (stream-map (compose score-corrupted cadr)
+	      (stream-filter (lambda (r) (eq? 'corrupted (car r)))
 		      (stream-map validate xs)))))
 
 (printf "part 1: ~a\n"
 	(with-data part1))
+
+(define (score-incomplete unmatched)
+  (define/match (f c)
+    [(#\() 1]
+    [(#\[) 2]
+    [(#\{) 3]
+    [(#\<) 4])
+  (foldl (lambda (c acc) (+ (* 5 acc) (f c))) 0 unmatched))
+
+(define (part2 xs)
+  (define (middle xs)
+    (list-ref xs (quotient (length xs) 2)))
+  (middle (sort
+	    (stream->list (stream-map (compose score-incomplete cadr)
+				      (stream-filter (lambda (r) (eq? 'incomplete (car r)))
+						     (stream-map validate xs))))
+	    <)))
+
+(printf "part 2: ~a\n"
+	(with-data part2))
