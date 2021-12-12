6356b3844e7d554084a8c699bbe2dd98b2d87a6d
Ben Price <ben@brprice.uk>
2021-12-12 17:47:11 +0000

    2021: racket day 12b

--- 2021/racket/day12.rkt
+++ 2021/racket/day12.rkt
@@ -28,12 +28,13 @@
 (define-syntax-rule (push! x xs)
   (set! xs (cons x xs)))
 
+(define (big-cave n)
+  (eq? n (string->symbol (string-upcase (symbol->string n)))))
+
 ; simple-minded brute force, actually finding all paths
-(define (paths g)
+(define (paths-helper g can-move)
   (let*
-    ([big-cave (lambda (n) (eq? n (string->symbol (string-upcase (symbol->string n)))))]
-     [can-move (lambda (p n) (or (big-cave n) (not (member n p))))]
-     [nshelp (lambda (x) (lambda (e) (if (eq? x (car e)) (cdr e) (car e))))]
+    ([nshelp (lambda (x) (lambda (e) (if (eq? x (car e)) (cdr e) (car e))))]
      [ns (lambda (x)
 	   (map (nshelp x)
 		(filter (lambda (e) (or (eq? (car e) x) (eq? (cdr e) x)))
@@ -50,8 +51,28 @@
 	       (when (can-move p n)
 		 (push! (cons n p) searching))))))))
 
+(define (paths g)
+  (paths-helper
+    g
+    (lambda (p n) (or (big-cave n) (not (member n p))))))
+
 (define (part1 xs)
   (length (paths xs)))
 
 (printf "part 1: ~a\n"
 	(with-data part1))
+
+(define (paths2 g)
+  (paths-helper
+    g
+    (lambda (p n) (or (big-cave n)
+		      (not (member n p))
+		      (and (not (eq? n 'start))
+			   (not (eq? n 'end))
+			   (not (check-duplicates (filter (compose not big-cave) p))))))))
+
+(define (part2 xs)
+  (length (paths2 xs)))
+
+(printf "part 2: ~a\n"
+	(with-data part2))
