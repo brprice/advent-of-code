e8b0a028b426e28d7094ee65d1ea9d4e9982341b
Ben Price <ben@brprice.uk>
2024-12-05 21:50:00 +0000

    2024: Haskell day 5a

---
+++ 2024/haskell/Day5.hs
@@ -0,0 +1,65 @@
+module Main where
+
+import Data.Map.Strict qualified as M
+import Data.Maybe (mapMaybe)
+import Data.Set qualified as S
+
+getData :: IO String
+getData = readFile "../data/day5"
+
+type Page = Int
+
+data Constr = C Page Page
+  deriving (Show)
+
+newtype Run = Run [Page]
+  deriving (Show)
+
+parse :: String -> ([Constr], [Run])
+parse s =
+  let ls = lines s
+      (cs, "" : rs) = break (== "") ls
+      parseConstr c =
+        let (l, '|' : r) = break (== '|') c
+         in C (read l) (read r)
+      parseRun r =
+        let (p, rs) = break (== ',') r
+         in read p : case rs of
+              [] -> []
+              ',' : ps -> parseRun ps
+   in (map parseConstr cs, map (Run . parseRun) rs)
+
+data RunInfo
+  = RI {len :: Int, pages :: S.Set Page, forbiddenLater :: S.Set Page}
+  | Invalid
+
+instance Semigroup RunInfo where
+  Invalid <> b = Invalid
+  a <> Invalid = Invalid
+  RI ll pl fll <> RI lr pr flr =
+    if S.disjoint fll pr
+      then RI (ll + lr) (pl <> pr) (fll <> flr)
+      else Invalid
+
+instance Monoid RunInfo where
+  mempty = RI 0 mempty mempty
+
+info :: M.Map Page (S.Set Page) -> Run -> RunInfo
+info mustBeEarlier (Run ps) = foldMap (\p -> RI 1 (S.singleton p) (M.findWithDefault mempty p mustBeEarlier)) ps
+
+mkEarlier :: [Constr] -> M.Map Page (S.Set Page)
+mkEarlier = M.unionsWith S.union . map (\(C l r) -> M.singleton r (S.singleton l))
+
+part1 :: ([Constr], [Run]) -> Int
+part1 (cs, rs) =
+  let earlier = mkEarlier cs
+      f r@(Run ps) = case info earlier r of
+        Invalid -> Nothing
+        RI l _ _ -> Just $ ps !! (div l 2)
+   in sum $ mapMaybe f rs
+
+main :: IO ()
+main = do
+  xs <- parse <$> getData
+  putStrLn "Part 1"
+  print $ part1 xs
