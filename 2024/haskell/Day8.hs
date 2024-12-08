43a13074aad0979e4b9fb549f48464f2bde2274b
Ben Price <ben@brprice.uk>
2024-12-08 12:32:16 +0000

    2024: Haskell day 8b

--- 2024/haskell/Day8.hs
+++ 2024/haskell/Day8.hs
@@ -31,8 +31,25 @@ part1 Grid {width, height, cts} =
       inBounds (x, y) = 0 <= x && x < width && 0 <= y && y < height
    in length $ S.fromList $ filter inBounds $ concatMap locs $ M.keys csLoc
 
+part2 :: Grid Char -> Int
+part2 Grid {width, height, cts} =
+  let csLoc = invertMap cts
+      locs' (x1, y1) (x2, y2) =
+        let dx = x2 - x1
+            dy = y2 - y1
+            ms = map (\n -> (x1 - n*dx, y1 - n*dy)) [0..]
+            ps = map (\n -> (x2 + n*dx, y2 + n*dy)) [0..]
+         in (takeWhile inBounds ms) ++ (takeWhile inBounds ps)
+      locs c =
+        let poss = S.elems $ csLoc M.! c
+         in concat [locs' p1 p2 | p1 <- poss, p2 <- poss, p1 /= p2]
+      inBounds (x, y) = 0 <= x && x < width && 0 <= y && y < height
+   in length $ S.fromList $ concatMap locs $ M.keys csLoc
+
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+  putStrLn "Part 2"
+  print $ part2 xs
