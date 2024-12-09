3da91821421369503daa4e3a11e1e0959af76617
Ben Price <ben@brprice.uk>
2024-12-09 10:34:46 +0000

    2024: Haskell day 9b
    This is terrible code, and takes ~6s compiled. There are some obvious
    optimizations but I am not going to bother doing them.

--- 2024/haskell/Day9.hs
+++ 2024/haskell/Day9.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Data.List (intersperse)
@@ -19,10 +21,14 @@ parse s =
       cs = intersperse Free $ map File [0 ..]
    in zipWith B lens cs
 
-part1 :: [Block] -> Int
-part1 bs = fst $ foldl' sumup (0, 0) $ go bs (reverse bs)
+sumup :: [(Int, Int)] -> Int
+sumup = fst . foldl' go (0, 0)
   where
-    sumup (acc, pos) (n, f) = (acc + ((n * (2 * pos + n - 1) * f) `div` 2), pos + n)
+    go (acc, pos) (n, f) = (acc + ((n * (2 * pos + n - 1) * f) `div` 2), pos + n)
+
+part1 :: [Block] -> Int
+part1 bs = sumup $ go bs (reverse bs)
+  where
     go (x : xs) (B n Free : ys) = go (x : xs) ys
     go (B n (File f) : xs) (y : ys) = case y of
       B m (File g) | f == g -> [(m, f)]
@@ -31,8 +37,25 @@ part1 bs = fst $ foldl' sumup (0, 0) $ go bs (reverse bs)
       | n <= m = (n, f) : go xs (B (m - n) (File f) : ys)
       | otherwise = (m, f) : go (B (n - m) Free : xs) ys
 
+part2 :: [Block] -> Int
+part2 bs = sumup $ go bs (reverse bs)
+  where
+    go (x : xs) (B n Free : ys) = go (x : xs) ys
+    go (B n (File f) : xs) ys = (n, f) : go xs (takeWhile (\case B _ Free -> True; B _ (File g) -> g > f) ys)
+    go (B n Free : xs) ys =
+      case break (\case B m Free -> False; B m (File _) -> m <= n) ys of
+        (_, []) -> (n, 0) : go xs ys
+        (ys1, B m (File f) : ys2) ->
+          (m, f)
+            : go
+              (B (n - m) Free : map (\case B m (File g) | g == f -> B m Free; x -> x) xs)
+              (ys1 ++ ys2)
+    go [] [] = []
+
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+  putStrLn "Part 2"
+  print $ part2 xs
