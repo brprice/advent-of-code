a583395d23ae975ecea2987b9180cd60f0a09d07
Ben Price <ben@brprice.uk>
2024-12-10 09:36:43 +0000

    2024: Haskell day 10b

--- 2024/haskell/Day10.hs
+++ 2024/haskell/Day10.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Data.Char (digitToInt)
@@ -12,21 +14,35 @@ getData = readFile "../data/day10"
 parse :: String -> Grid Int
 parse = parseGrid (pure . digitToInt)
 
+nbdCoords :: (Int, Int) -> [(Int, Int)]
+nbdCoords (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+
+dp :: (((Int, Int), Int, [(Int, a)]) -> a) -> Grid Int -> Grid (Int, a)
+dp f g@Grid {cts = m} =
+  let cts = M.mapWithKey (\k v -> (v, f (k, v, mapMaybe (cts M.!?) $ nbdCoords k))) m
+   in g {cts}
+
+trailHeads :: Grid (Int, a) -> [a]
+trailHeads = map snd . filter ((== 0) . fst) . M.elems . cts
+
 part1 :: Grid Int -> Int
-part1 Grid {cts = m} =
-  let nbd (x, y) = mapMaybe (flip M.lookup reachable) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-      r k v =
-        ( m M.! k,
-          case v of
-            9 -> S.singleton k
-            n -> S.unions $ map snd $ filter ((== n + 1) . fst) $ nbd k
-        )
-      reachable = M.mapWithKey r m
-      trailHeads = filter ((== 0) . fst) $ M.elems reachable
-   in sum $ map (S.size . snd) trailHeads
+part1 = sum . map S.size . trailHeads . dp f
+  where
+    f = \case
+      (p, 9, _) -> S.singleton p
+      (_, n, nbd) -> S.unions $ map snd $ filter ((== n + 1) . fst) nbd
+
+part2 :: Grid Int -> Int
+part2 = sum . trailHeads . dp f
+  where
+    f = \case
+      (p, 9, _) -> 1
+      (_, n, nbd) -> sum $ map snd $ filter ((== n + 1) . fst) nbd
 
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+  putStrLn "Part 2"
+  print $ part2 xs
