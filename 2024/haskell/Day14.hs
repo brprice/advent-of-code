6e8282553c047ff48496c41d0ce97227aa286599
Ben Price <ben@brprice.uk>
2024-12-14 14:20:24 +0000

    2024: Haskell day 14b

--- 2024/haskell/Day14.hs
+++ 2024/haskell/Day14.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main where
+
+import Control.Monad (void)
+import Data.List (sort)
+import Data.Map qualified as M
 
 getData :: IO String
 getData = readFile "../data/day14"
@@ -46,8 +51,43 @@ safetyFactor (ul, ur, ll, lr) = length ul * length ur * length ll * length lr
 part1 :: [Robot] -> Int
 part1 = safetyFactor . quadrants . map (step 100)
 
+concentration :: Integer -> [Robot] -> Integer
+concentration nbdRadius =
+  let nbd (x, y) = [(x + dx, y + dy) | dx <- [-nbdRadius .. nbdRadius], dy <- [-nbdRadius .. nbdRadius]]
+   in \rs ->
+        let ps = M.fromListWith (+) $ map ((,1) . pos) rs
+         in sum $ map (\p -> sum $ [M.findWithDefault 0 n ps | n <- nbd p]) $ M.keys ps
+
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+
+  let mkLine x rxs
+        | x == width = ""
+        | otherwise = case rxs of
+            [] -> replicate (fromIntegral $ width - x) '.'
+            rx : rxs'
+              | x == rx -> '#' : mkLine (x + 1) rxs'
+              | otherwise -> '.' : mkLine (x + 1) rxs
+      draw1 l xs
+        | l == height = pure ()
+        | otherwise =
+            let (map snd -> thisLine, rest) = span ((== l) . fst) xs
+             in do
+                  putStrLn $ mkLine 0 thisLine
+                  draw1 (l + 1) rest
+      draw xs = draw1 0 $ sort $ map (\R {pos = (x, y)} -> (y, x)) xs
+      go best s ys =
+        let c = concentration 2 ys
+         in if c > best
+              then do
+                draw ys
+                putStrLn $ "New high point (pictured above): step " ++ show s ++ "\t has concentration " ++ show c ++ "\t(press enter to continue)"
+                void getLine
+                go c (s + 1) (map (step 1) ys)
+              else go best (s + 1) (map (step 1) ys)
+  putStrLn "Part 2 is interactive! Press enter to start"
+  void getLine
+  go 0 0 xs
