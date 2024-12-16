123214958caaee957bbdac9c524f0d8a5c0807f0
Ben Price <ben@brprice.uk>
2024-12-16 00:22:45 +0000

    2024: Haskell day 15b

--- 2024/haskell/Day15.hs
+++ 2024/haskell/Day15.hs
@@ -5,6 +5,7 @@ module Main where
 import Data.List (foldl')
 import Data.Map.Strict qualified as M
 import Data.Monoid (Sum (Sum, getSum))
+import Data.Set qualified as S
 import Utils (Grid (Grid, cts), parseGrid)
 
 getData :: IO String
@@ -58,8 +59,66 @@ part1 (w, dirs) = getSum $ foldMap gps $ M.toList $ obstructions $ foldl' step w
         ((x, y), Box) -> 100 * y + x
         (_, Wall) -> 0
 
+data Box2 = L | R
+
+data Obstr2 = Box2 Box2 | Wall2
+
+data Warehouse2 = W2 {robot2 :: !(Int, Int), obstructions2 :: M.Map (Int, Int) Obstr2}
+
+step2 :: Warehouse2 -> (Int, Int) -> Warehouse2
+step2 w@W2 {robot2, obstructions2} dir =
+  let next = robot2 +. dir
+      bothHalves p = \case
+        L -> [(p, L), (p +. (1, 0), R)]
+        R -> [(p, R), (p +. (-1, 0), L)]
+      -- one-step "what boxes are pushed on" (Nothing if a wall is)
+      sb :: [((Int, Int), Box2)] -> Maybe [((Int, Int), Box2)]
+      sb =
+        fmap concat
+          . traverse
+            ( \(p, _) -> case obstructions2 M.!? (p +. dir) of
+                Nothing -> Just []
+                Just Wall2 -> Nothing
+                Just (Box2 b) -> Just $ filter ((/= p) . fst) $ bothHalves (p +. dir) b
+            )
+      -- multi-step unfolding of sb
+      affected :: [((Int, Int), Box2)] -> Maybe [((Int, Int), Box2)]
+      affected = \case
+        [] -> Just []
+        b -> fmap (b ++) $ affected =<< sb b
+   in case obstructions2 M.!? next of
+        Nothing -> w {robot2 = next}
+        Just Wall2 -> w
+        Just (Box2 b) -> case affected $ bothHalves next b of
+          Nothing -> w
+          Just bs ->
+            W2 next $
+              M.union (M.fromList $ map (\(p, b) -> (p +. dir, Box2 b)) bs) $
+                M.withoutKeys obstructions2 (S.fromList $ map fst bs)
+
+part2 :: (Warehouse, [(Int, Int)]) -> Int
+part2 (W {robot = (rx, ry), obstructions}, dirs) =
+  let w2 =
+        W2 (rx * 2, ry) $
+          M.fromList $
+            M.foldMapWithKey
+              ( \(x, y) -> \case
+                  Box -> [((2 * x, y), Box2 L), ((2 * x + 1, y), Box2 R)]
+                  Wall -> [((2 * x, y), Wall2), ((2 * x + 1, y), Wall2)]
+              )
+              obstructions
+   in getSum $ foldMap gps $ M.toList $ obstructions2 $ foldl' step2 w2 dirs
+  where
+    gps =
+      Sum . \case
+        ((x, y), Box2 L) -> 100 * y + x
+        (_, Box2 R) -> 0
+        (_, Wall2) -> 0
+
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+  putStrLn "Part 2"
+  print $ part2 xs
