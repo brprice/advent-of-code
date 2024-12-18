bbeaae803ce3e51b7b573473d835f306fd46c217
Ben Price <ben@brprice.uk>
2024-12-18 20:42:55 +0000

    2024: Haskell refactor to prep for day 18b

--- 2024/haskell/Day12.hs
+++ 2024/haskell/Day12.hs
@@ -7,50 +7,13 @@ import Data.Map.Strict qualified as M
 import Data.Maybe (isJust)
 import Data.Monoid (Dual (Dual, getDual), Endo (Endo, appEndo), Sum (Sum))
 import Data.Set qualified as S
-import Utils (Grid (cts, height, width), parseGrid)
+import Utils (CommutativeMonoid, Grid (cts, height, width), empty, find, insert, parseGrid, union)
 
 getData :: IO String
 getData = readFile "../data/day12"
 
 parse :: String -> Grid Char
 parse = parseGrid Just
-
-class (Monoid m) => CommutativeMonoid m
-
--- commutative-monoidal decorated union find
--- NB: I suspect this is pretty inefficient (in particular, part 1 was much slower than I expected)
-data UnionFind' m a = Root !Int !m | Parent !a
-  deriving (Show)
-
-newtype UnionFind m a = UF (M.Map a (UnionFind' m a))
-  deriving (Show)
-
-empty :: UnionFind m a
-empty = UF M.empty
-
-insert :: (Ord a) => m -> a -> UnionFind m a -> UnionFind m a
-insert m a (UF u) = UF $ M.insertWith (error "key already exists") a (Root 1 m) u
-
--- find also "mutates" the structure, by "compressing paths"
--- It returns (size of set,decoration-of-set,representative-of-set) as well as mutated structure
-find :: (Ord a) => a -> UnionFind m a -> (Int, m, a, UnionFind m a)
-find a uf@(UF u) = case u M.! a of
-  Root s m -> (s, m, a, uf)
-  Parent b -> case u M.! b of
-    Root s m -> (s, m, b, uf)
-    Parent c -> find c $ UF $ M.insert a (Parent c) u
-
-union :: (Ord a, CommutativeMonoid m) => a -> a -> UnionFind m a -> UnionFind m a
-union a b u0 =
-  let (sa, ma, ra, u1) = find a u0
-      (sb, mb, rb, u2@(UF u2')) = find b u1
-      (ra', rb') =
-        if sa < sb
-          then (Parent rb, Root (sa + sb) (ma <> mb))
-          else (Root (sa + sb) (ma <> mb), Parent ra)
-   in if ra == rb then u2 else UF $ M.insert ra ra' $ M.insert rb rb' u2'
-
-instance CommutativeMonoid (Sum Int)
 
 -- returns set of (coord-of-some-field-in-the-region, (size of region, decoration on region))
 getAreas :: (CommutativeMonoid m, Ord m) => ((Int, Int) -> Char -> [Maybe Char] -> m) -> Grid Char -> S.Set ((Int, Int), (Int, m))
--- 2024/haskell/Utils.hs
+++ 2024/haskell/Utils.hs
@@ -5,12 +5,19 @@ module Utils
     parseGrid,
     invertMap,
     shortestPathLengths,
+    CommutativeMonoid,
+    UnionFind,
+    empty,
+    insert,
+    find,
+    union,
   )
 where
 
 import Data.Bifunctor (first)
 import Data.Map qualified as M
 import Data.Maybe (mapMaybe)
+import Data.Monoid (Sum)
 import Data.Set qualified as S
 import Numeric.Natural (Natural)
 
@@ -71,3 +78,40 @@ shortestPathLengths nbd start = go S.empty (singleton 0 start)
                       map (first (+ c)) $
                         nbd s
                 )
+
+class (Monoid m) => CommutativeMonoid m
+
+-- commutative-monoidal decorated union find
+-- NB: I suspect this is pretty inefficient (in particular, Day 12 part 1 was much slower than I expected)
+data UnionFind' m a = Root !Int !m | Parent !a
+  deriving (Show)
+
+newtype UnionFind m a = UF (M.Map a (UnionFind' m a))
+  deriving (Show)
+
+empty :: UnionFind m a
+empty = UF M.empty
+
+insert :: (Ord a) => m -> a -> UnionFind m a -> UnionFind m a
+insert m a (UF u) = UF $ M.insertWith (error "key already exists") a (Root 1 m) u
+
+-- find also "mutates" the structure, by "compressing paths"
+-- It returns (size of set,decoration-of-set,representative-of-set) as well as mutated structure
+find :: (Ord a) => a -> UnionFind m a -> (Int, m, a, UnionFind m a)
+find a uf@(UF u) = case u M.! a of
+  Root s m -> (s, m, a, uf)
+  Parent b -> case u M.! b of
+    Root s m -> (s, m, b, uf)
+    Parent c -> find c $ UF $ M.insert a (Parent c) u
+
+union :: (Ord a, CommutativeMonoid m) => a -> a -> UnionFind m a -> UnionFind m a
+union a b u0 =
+  let (sa, ma, ra, u1) = find a u0
+      (sb, mb, rb, u2@(UF u2')) = find b u1
+      (ra', rb') =
+        if sa < sb
+          then (Parent rb, Root (sa + sb) (ma <> mb))
+          else (Root (sa + sb) (ma <> mb), Parent ra)
+   in if ra == rb then u2 else UF $ M.insert ra ra' $ M.insert rb rb' u2'
+
+instance CommutativeMonoid (Sum Int)
