ac3e03e97800a9746f5b7541feb97b48ac4976db
Ben Price <ben@brprice.uk>
2024-12-22 12:03:33 +0000

    2024: Haskell day 22b
    On the slow side, ~8s compiled.

--- 2024/haskell/Day22.hs
+++ 2024/haskell/Day22.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.Bits (Bits (xor))
+import Data.List (tails)
+import Data.Map.Strict qualified as M
 
 getData :: IO String
 getData = readFile "../data/day22"
@@ -18,8 +20,23 @@ step = step' (* 2048) . step' (`div` 32) . step' (* 64)
 part1 :: [Integer] -> Integer
 part1 = sum . map ((!! 2000) . iterate step)
 
+part2 :: [Integer] -> Integer
+part2 = maximum . M.elems . M.unionsWith (+) . map part2'
+  where
+    part2' = priceChgRuns . map (`mod` 10) . iterate step
+    priceChgs ps = zipWith (\p1 p2 -> (p2 - p1, p2)) ps (tail ps)
+    priceChgRuns' =
+      map (\dps -> (map fst dps, snd $ last dps))
+        . take (2000 - 3)
+        . map (take 4)
+        . tails
+        . priceChgs
+    priceChgRuns = M.fromListWith (\new old -> old) . priceChgRuns'
+
 main :: IO ()
 main = do
   xs <- parse <$> getData
   putStrLn "Part 1"
   print $ part1 xs
+  putStrLn "Part 2"
+  print $ part2 xs
